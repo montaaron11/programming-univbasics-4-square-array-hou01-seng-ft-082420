89d51e2a524481fbd82558c5ca5a9b080fe9a681
Aaron Montgomery <montaaron11@gmail.com>
2020-07-23 07:27:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(number)
 new_array= []
 counter = 0 
   while counter < numbers.length do
-    new_array << 
+    new_array << (number[counter]*number[counter])
     counter += 1
   end
 end
